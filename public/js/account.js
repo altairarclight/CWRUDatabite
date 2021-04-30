a34d08d83611bfdf9e117eb3659ad8c3d4aba24d
altairarclight <79873554+altairarclight@users.noreply.github.com>
2021-04-30 03:51:43 -0400

    optimize password check
    check old password in the last conditional to reduce unneeded server requests

--- public/js/account.js
+++ public/js/account.js
@@ -6,17 +6,16 @@ function passwordValidateSuccess() {
   let newPass = dom('#newPassword')
   let newPass2 = dom('#newPassword2')
   let errorPrompt = dom('#passwordError')
-  if (oldPass.value != "test") {
-    errorPrompt.innerText = "Incorrect password"
-    oldPass.classList.toggle('active')
-    return false
-  }
   if (newPass.value != newPass2.value) {
     errorPrompt.innerText = "Passwords do not match"
     return false
   }
   if (newPass.value.length < 8 || newPass2.value.length < 8) {
     errorPrompt.innerText = "New password must be at least 8 characters"
+    return false
+  }
+  if (oldPass.value != "test") {
+    errorPrompt.innerText = "Incorrect password"
     return false
   }
   return true
