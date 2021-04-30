b5665fa642b326513b8d55cbb412ab3e38af475a
jxg716 <jxg716@case.edu>
2021-04-30 01:39:00 -0400

    Add files via upload
    Added functionality for checkFileType() method. This included adding several helper methods.

--- Backend/Implementation/FileHandler.java
+++ Backend/Implementation/FileHandler.java
@@ -1,15 +1,39 @@
-package Implementation;
+import java.io.File;
 
+package Implementation;
 
 public class FileHandler {
 
 	Object file;
 	
-	public void checkFileType(Object File) {
+	public void checkFileType(File file) {
+		String fileName = file.getName();
+		
+		String extension = getExtension(fileName);
+		if(extension.equals("CSV") ==  false && extension.equals("TSV") ==  false &&
+		   extension.equals("txt") ==  false && extension.equals("dat") ==  false &&
+		   extension.equals("sql") ==  false && extension.equals("xml") ==  false ){
+				System.out.println("Error. Incorrect file format.");
+		}
 		
 	}
 	
 	public void convertToFile(Object dataSet, Object fileType) {
 		
+	}
+	
+	private String getExtension(String name) {
+		int position = getExtensionPosition(name);
+		return name.substring(position, name.length());
+	}
+	
+	private int getExtensionPosition(String name) {
+		int returnVal = 0;
+		for(int i =0; i< name.length();i++) {
+			if (name.charAt(i) == '.') {
+				returnVal = i;
+			}
+		}
+		return returnVal;
 	}
 }
